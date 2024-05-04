2a74f42d66bdfd563b18e7cde497f3454d086c17
M0hamedSafwat <168871444+M0hamedSafwat@users.noreply.github.com>
2024-05-04 06:31:31 +0300

    Update Mo_Scripts.py

--- Mo_Scripts.py
+++ Mo_Scripts.py
@@ -31,16 +31,10 @@ light_yellow_color = '\033[1;33m'
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 m500 = 'https://raw.githubusercontent.com/Mohamedsafwat0/.github/main/500Mg.py'
-spam = 'https://raw.githubusercontent.com/junku1/junku1/main/spam%20whats%20.py'
-spam2 = 'https://raw.githubusercontent.com/junku1/junku1/main/spam%20sms.py'
-like = 'https://raw.githubusercontent.com/junku1/junku1/main/%D9%84%D9%8A%D9%83%D8%A7%D8%AA%20%D8%A7%D9%86%D8%B3%D8%AA%D8%AC%D8%B1%D8%A7%D9%85.py'
-views = 'https://raw.githubusercontent.com/junku1/junku1/main/views%20TiK%20Tok.py'
-dvoda = 'https://raw.githubusercontent.com/junku1/junku1/main/data%20nu%20voda%20(jo).py'
-Etislat = 'https://raw.githubusercontent.com/junku1/junku1/main/%D8%B3%D8%A7%D8%B9%D8%AA%D9%8A%D9%86%20%D8%A7%D8%AA%D8%B5%D8%A7%D9%84%D8%A7%D8%AA.py'
-id = 'https://raw.githubusercontent.com/junku1/junku1/main/%D8%A8%D9%8A%D8%A7%D9%86%D8%A7%D8%AA.py'
-Followers = 'https://raw.githubusercontent.com/junku1/junku1/main/%D9%85%D8%AA%D8%A7%D8%A8%D8%B9%D9%8A%D9%86.py'
-Spotify = 'https://raw.githubusercontent.com/junku1/junku1/main/%D8%A7%D8%B4%D8%AA%D8%B1%D8%A7%D9%83spoyifay.py'
-Watchit="https://raw.githubusercontent.com/junku1/junku1/main/Watchit%20Subscription.py"
+spam = 'https://raw.githubusercontent.com/Mohamedsafwat0/.github/main/Spam_Whatsapp.py'
+spam2 = 'https://raw.githubusercontent.com/Mohamedsafwat0/.github/main/SpamforAll.py'
+Etislat = 'https://raw.githubusercontent.com/Mohamedsafwat0/.github/main/2H_etisalat.py'
+
 def main():
     clear_screen()
     
@@ -102,27 +96,21 @@ print(B+"""
 [1] 500 mega Orange
 [2] spam WhatsApp 
 [3] spam sms all Network 
-[4] Free insta likes 
-[5] Free TikTok views 
-[6] Vodafone number data 
-[7] Two hours of free social Etislat
-[8] National ID information 
-[9] 125 insta followers for free 
-[10] Spotify Orange subscription 
-[11] Free Watchit subscription to Orange 
+[4] Two hours Etisalat of free social  
+ 
 """)
 number = int(input(Y+"Choose the script: "))
 clear_screen()  # مسح الشاشة بعد اختيار المستخدم لرقم
 if number == 1:
-        jo = m500
+        Mo = m500
 elif number == 2:
-        jo = spam
+        Mo = spam
 elif number == 3:
-        jo = spam2
+        Mo = spam2
 elif number == 4:
-        jo = like
+        Mo = Etislat
 elif number == 5:
-        jo = views
+        Mo = views
 elif number == 6:
         jo = dvoda
 elif number == 7:
